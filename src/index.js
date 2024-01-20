67affae43d82b0e4528fb467c305d3acb58b4e76
Logesh Suresh <logeshsrinivasans@gmail.com>
2024-01-21 02:01:33 +0530

    Implemented queues to consume message

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const amqplib = require('amqplib');
 
 const { ServerConfig } = require('./config');
+const { EmailService } = require('./services');
 const apiRoutes = require('./routes');
 
 const app = express();
@@ -10,6 +12,28 @@ app.use(express.urlencoded({extended: true}));
 
 app.use('/api', apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+async function connectQueueAndSendMail() {
+    try {
+        const connection = await amqplib.connect('amqp://localhost');
+        const channel = await connection.createChannel();
+        await channel.assertQueue('noti-queue');
+        await channel.consume('noti-queue', (data) => {
+            console.log(`${Buffer.from(data.content)}`);
+            const { recipientEmail, subject, text } = JSON.parse(Buffer.from(data.content));
+            console.log(recipientEmail);
+            console.log(subject);
+            console.log(text);
+            EmailService.sendEmail(ServerConfig.GMAIL_EMAIL, recipientEmail, subject, text);
+            channel.ack(data);
+        });
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+app.listen(ServerConfig.PORT, async () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+    await connectQueueAndSendMail();
+    console.log('queue is up !');
 });
